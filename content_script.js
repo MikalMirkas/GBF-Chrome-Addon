85a322f58cebe47f093a42f7bd42c350c975fee3
MikalMirkas <MikalMirkas@users.noreply.github.com>
2016-07-11 03:33:30 -0500

    Pruned file, saving it for a later date.

--- content_script.js
+++ content_script.js
@@ -1,26 +1,9 @@
-//depricated file
-
 var node = document.querySelector('.column-type-search p.js-tweet-text');
-var re = /ID：(\w+)/;
 
-for(var i in node.childNodes) {
-	var cnode = node.childNodes[i];
-	var text = cnode.textContent;
-	if(match = re.exec(cnode.textContent)) {
-		var pos = re.exec(text).index;
-		var st = pos+3;
-		var ed = st+8;
-		var range = document.createRange();
-		range.setStart(cnode,st);
-		range.setEnd(cnode,ed);
-		var selection = window.getSelection();
-		selection.removeAllRanges();
-		selection.addRange(range);
-		chrome.runtime.sendMessage({data: "data"});
-
-		var success = document.execCommand('copy');  
-		chrome.runtime.sendMessage({title: range.toString(), message: text}, function(){});
-		break;
-	}
-}
-
+/* document.addEventListener('selectionchange', function() {
+    var selection = window.getSelection().toString().trim();
+    chrome.runtime.sendMessage({
+        request: 'updateContextMenu',
+        selection: selection
+    });
+}); */
